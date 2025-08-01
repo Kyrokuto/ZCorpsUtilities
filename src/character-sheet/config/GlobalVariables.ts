51c006d7827106b56f56413dada4a1dec1bae1c9
Kyrokuto [BZH] <tleguillant@gmail.com>
2025-08-01 11:44:04 +0200

    [ZCorpsUtilities-#10] - Copy defaults global variables

---
+++ src/character-sheet/config/GlobalVariables.ts
@@ -0,0 +1,41 @@
+interface GlobalVariablesObject {
+    DEFAULT_SEPARATOR_ID: string;
+    DATA_KEY_SUPER_PREFIX_ID: string;
+    DATA_KEY_PREFIX_ID: string;
+    INPUT_NUMBER_DICE_CODE_SUFFIX: string;
+    INPUT_CHECK_ID_PLUS_ONE_SUFFIX: string;
+    INPUT_CHECK_ID_PLUS_TWO_SUFFIX: string;
+    ELEMENT_ID_SMALL_CHARACTER_SHEET_NAME: string;
+    ELEMENT_ID_BUTTON_LOAD_DATA: string;
+    ELEMENT_ID_BUTTON_SAVE_DATA: string;
+    ELEMENT_ID_BUTTON_RESET_DATA: string;
+    ELEMENT_ID_BUTTON_DELETE_DATA: string;
+    ELEMENT_ID_TOAST: string;
+    ELEMENT_ID_MODAL_GLOBAL: string;
+    ELEMENT_ID_MODAL_GLOBAL_LABEL: string;
+    INPUT_ID_CHARACTER_NAME: string;
+    INPUT_ID_CHARACTER_DATA: string;
+    ELEMENT_ID_BUTTON_FIND_SKILL_FEATURE: string;
+    INPUT_SELECT_FIND_SKILL_FEATURE: string;
+}
+
+export const GlobalVariables: GlobalVariablesObject = {
+    DEFAULT_SEPARATOR_ID: '-',
+    DATA_KEY_SUPER_PREFIX_ID: 'superPrefixId',
+    DATA_KEY_PREFIX_ID: 'prefixId',
+    INPUT_NUMBER_DICE_CODE_SUFFIX: 'dice-code-input',
+    INPUT_CHECK_ID_PLUS_ONE_SUFFIX: 'plus-one-input',
+    INPUT_CHECK_ID_PLUS_TWO_SUFFIX: 'plus-two-input',
+    ELEMENT_ID_SMALL_CHARACTER_SHEET_NAME: 'small-character-sheet-name',
+    ELEMENT_ID_BUTTON_LOAD_DATA: 'button-load-data',
+    ELEMENT_ID_BUTTON_SAVE_DATA: 'button-save-data',
+    ELEMENT_ID_BUTTON_RESET_DATA: 'button-reset-data',
+    ELEMENT_ID_BUTTON_DELETE_DATA: 'button-delete-data',
+    ELEMENT_ID_TOAST: 'toast-message',
+    ELEMENT_ID_MODAL_GLOBAL: 'modal-global',
+    ELEMENT_ID_MODAL_GLOBAL_LABEL: 'modal-global-label',
+    INPUT_ID_CHARACTER_NAME: 'character-name',
+    INPUT_ID_CHARACTER_DATA: 'character-data',
+    ELEMENT_ID_BUTTON_FIND_SKILL_FEATURE: 'button-find-skill-feature',
+    INPUT_SELECT_FIND_SKILL_FEATURE: 'select-find-skill-feature',
+}
