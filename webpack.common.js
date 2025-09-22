3a19461400763c381fdef9e3c6989b55eabcc12b
Kyrokuto [BZH] <tleguillant@gmail.com>
2025-09-22 16:08:41 +0200

    [ZCorpsUtilities-#10] - Add "terser-webpack-plugin" on dev

--- webpack.common.js
+++ webpack.common.js
@@ -13,6 +13,7 @@ module.exports = {
   resolve: {
     extensions: ['.ts', '.tsx', '.js'],
   },
+
   module: {
     rules: [
       {
--- webpack.prod.js
+++ webpack.prod.js
@@ -2,9 +2,15 @@
 const { merge } = require('webpack-merge')
 // eslint-disable-next-line @typescript-eslint/no-require-imports,no-undef
 const common = require('./webpack.common.js')
+// eslint-disable-next-line @typescript-eslint/no-require-imports,no-undef
+const TerserPlugin = require('terser-webpack-plugin')
 
 // eslint-disable-next-line no-undef
 module.exports = merge(common, {
   mode: 'production',
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()],
+  },
 })
 
